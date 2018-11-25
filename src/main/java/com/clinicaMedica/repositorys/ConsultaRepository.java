80363634ff1ba103218efcc1e3280adcf628ec1a
Wagner Paulino <wagnersantos9778@gmail.com>
2018-11-25 20:03:34 -0300

    ajustes

--- src/main/java/com/clinicaMedica/repositorys/ConsultaRepository.java
+++ src/main/java/com/clinicaMedica/repositorys/ConsultaRepository.java
@@ -10,7 +10,7 @@ import com.clinicaMedica.projections.CountConsultaProximosDias;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
-	@Query(" select count(c.id) as count, c.dtConsulta as dtConsulta from Consulta c where c.dtConsulta > CURRENT_DATE and coalesce(c.realizada,false) = false group by c.dtConsulta ")
+	@Query(" select count(c.id) as count, c.dtConsulta as dtConsulta from Consulta c where c.dtConsulta > CURRENT_DATE and coalesce(c.realizada,false) = false group by c.dtConsulta order by c.dtConsulta asc ")
 	public List<CountConsultaProximosDias> countConsultasProximosDias();
 
 }
