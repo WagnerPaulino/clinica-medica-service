f474fe780a1f099975170149f2ec0537e59f8983
Wagner Paulino <wagnersantos9778@gmail.com>
2018-12-09 22:30:14 -0300

    Ajustes no sql do relatorio

--- src/main/java/com/clinicaMedica/repositorys/ConsultaRepository.java
+++ src/main/java/com/clinicaMedica/repositorys/ConsultaRepository.java
@@ -14,7 +14,7 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 	@Query(" select count(c.id) as count, c.dtConsulta as dtConsulta from Consulta c where cast(c.dtConsulta as date) > cast(CURRENT_DATE as date) and coalesce(c.realizada,false) = false group by c.dtConsulta order by c.dtConsulta asc ")
 	public List<CountConsultaProximosDias> countConsultasProximosDias();
 	
-	@Query(" select c from Consulta c where c.dtConsulta between ?1 and ?2 or c.dtRetorno between ?3 and ?4 ")
+	@Query(" select c from Consulta c where c.dtConsulta between ?1 and ?2 or c.dtRetorno between ?3 and ?4 ORDER BY c.dtConsulta DESC, c.dtRetorno")
 	public List<Consulta> findConsultaByPeriodo(LocalDate dtConsultaIni, LocalDate dtConsultaFim , LocalDate dtRetornoIni, LocalDate dtRetornoFim);
 
 }
